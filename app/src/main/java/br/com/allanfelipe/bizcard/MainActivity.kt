b12aa9cb3d4e191bd7a1d09b83c05fad2493f5f9
Allan <allanfelipedk@gmail.com>
2023-12-14 21:45:57 -0300

    Android Compose Surface Layout and Preview

---
+++ app/src/main/java/br/com/allanfelipe/bizcard/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.allanfelipe.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.allanfelipe.bizcard.ui.theme.BizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+    Surface (modifier = Modifier.fillMaxWidth().fillMaxHeight()){
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BizCardTheme {
+        CreateBizCard()
+    }
+}
